4e3cb7ba948898a344293cc95ae5a36332f83c74
erick Monteiro <monteiro.ck@hotmail.com>
2019-11-06 10:26:24 -0300

    acerto na validação

--- src/app/shared/campo-control-erro/campo-control-erro.component.ts
+++ src/app/shared/campo-control-erro/campo-control-erro.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CampoControlErroComponent implements OnInit {
 
-  @Input() monstrarErro: boolean;
+  @Input() mostrarErro: boolean;
   @Input() msgErro: string;
   constructor() { }
 
