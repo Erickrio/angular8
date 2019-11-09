0240c9418fd8a58a83cec0120e8abe6db9306146
erick Monteiro <monteiro.ck@hotmail.com>
2019-11-09 15:51:04 -0300

    validações do formulario - clicando no submit p campos que não forem modificados

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -15,59 +15,84 @@ export class DataFormComponent implements OnInit {
   constructor(private formBuilder: FormBuilder,
     private http: HttpClient
 
-    ) { }
+  ) { }
 
 
   ngOnInit() {
     //Inicio do componente
     this.formulario = this.formBuilder.group({
-      nome:[null,
+      nome: [null,
         Validators.required],
-        // Validators.minLength(3),
-        // Validators.maxLength(20)],
-        email:[null,[Validators.required,Validators.email]],
-        endereco: this.formBuilder.group({
-            cep:[null,Validators.required],
-            numero:[null,Validators.required],
-            complemento:[null],
-            rua:[null,Validators.required],
-            bairro:[null,Validators.required],
-            cidade:[null,Validators.required],
-            estado:[null,Validators.required]
+      // Validators.minLength(3),
+      // Validators.maxLength(20)],
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required]
       })
     });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.formulario);
-    this.http .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-    .subscribe(dados => { console.log(dados);
-      //reseta form
-      this.formulario.reset();
-      // this.resetar();
 
-    },
-    (error: any)=>alert('erro'));
+    if (this.formulario.valid) {
+      this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+        .subscribe(dados => {
+          console.log(dados);
+          //reseta form
+          this.formulario.reset();
+          // this.resetar();
+
+        },
+          (error: any) => alert('erro'));
+
+    } else {
+      console.log('formulario invalido')
+      this.verificaValidacoesForm(this.formulario);
+
+    }
+
+  }
+  // faz validações do formulario - apertando o submit p campos que não forem modificados
+
+  verificaValidacoesForm(formGroup: FormGroup)  {
+    Object.keys(formGroup.controls).forEach(campo => {
+      console.log(campo);
+      const controle = formGroup.get(campo);
+      controle.markAsDirty() // dirty - campo modificado
+      if (controle instanceof FormGroup){
+        // caso seja verificado as validações do form
+          this.verificaValidacoesForm(controle);
+
+      }
+    });
+
   }
 
-  resetar(){
+  resetar() {
     this.formulario.reset();
   }
 
 
-  verificaEmailInvalido(campo: string){
+  verificaEmailInvalido(campo: string) {
     let campoEmail = this.formulario.get('email');
-    if(campoEmail.errors){
+    if (campoEmail.errors) {
       return campoEmail.errors['email'] && campoEmail.touched;
     }
   }
 
-  verificaValidTouched(campo: string){
-   return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  verificaValidTouched(campo: string) {
+    return !this.formulario.get(campo).valid && (this.formulario.get(campo).touched || this.formulario.get(campo).dirty);
   }
 
 
-  aplicaCssErro(campo : string){
+  aplicaCssErro(campo: string) {
     return {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo)
@@ -80,55 +105,55 @@ export class DataFormComponent implements OnInit {
     let cep = this.formulario.get('endereco.cep').value;
 
 
-     // Nova variável "cep" somente com dígitos.
-     cep = cep.replace(/\D/g, '');
+    // Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
 
-     // Verifica se campo cep possui valor informado.
-     if (cep !== '') {
-       // Expressão regular para validar o CEP.
-       const validacep = /^[0-9]{8}$/;
+    // Verifica se campo cep possui valor informado.
+    if (cep !== '') {
+      // Expressão regular para validar o CEP.
+      const validacep = /^[0-9]{8}$/;
 
-       // Valida o formato do CEP.
-       if (validacep.test(cep)) {
-         this.resetaDadosFormulario();
-          this.http.get(`//viacep.com.br/ws/${cep}/json`)
-         // this.httpClient.get(`//viacep.com.br/ws/${cep}/json`)
-          .pipe(map(dados =>  dados))
+      // Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.resetaDadosFormulario();
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          // this.httpClient.get(`//viacep.com.br/ws/${cep}/json`)
+          .pipe(map(dados => dados))
           .subscribe(dados => this.populaDadosForm(dados))
-       }
-     }
+      }
+    }
 
-
-   }
-
-   populaDadosForm(dados){
-    this.formulario.patchValue({
-          endereco: {
-            rua: dados.logradouro ,
-            // cep: dados.cep ,
-            complemento: dados.complemento,
-            bairro: dados.bairro,
-            cidade: dados.localidade,
-            estado: dados.uf
-          }
-
-        });
 
   }
 
-  resetaDadosFormulario(){
+  populaDadosForm(dados) {
     this.formulario.patchValue({
-         endereco: {
-           rua: null ,
-           complemento: null,
-           bairro: null,
-           cidade:null,
-           estado:null
-         }
+      endereco: {
+        rua: dados.logradouro,
+        // cep: dados.cep ,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
 
-       });
+    });
 
- }
+  }
+
+  resetaDadosFormulario() {
+    this.formulario.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+
+    });
+
+  }
 
 }
 
