64495d20236fa0676b48d7c7979c591051cee13e
erick Monteiro <monteiro.ck@hotmail.com>
2019-11-25 15:09:23 -0300

    espaço entre botões no fomulario html ou no ts

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true // Opção 2 - Espaço entre botoes
 })
 export class CursosListaComponent implements OnInit {
 
